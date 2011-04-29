977deed3589ef3638cd8cc5e0cbd2660cd5fafed
Fidel Sosa <unpurposed@gmail.com>
2011-04-28 20:36:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skullweb::Application.config.secret_token = '08609c270c14647a4afed9643bc69f962c7a9f19a1ec37851b1812399f9ae45c086562a55b6b9aca5ae0f8335b80a078fe95cd02b043b35eb695670adda251bc'
