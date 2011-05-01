11c7879f4f54b04e42876da6d6c48962d05d59ff
Michael Patterson <mrpatte@mit.edu>
2011-05-01 15:00:41 -0400

    time actually works

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -29,10 +29,32 @@ def room_list
 end
 
 def create
-  date1 =Date.parse("#{params[:m1]}"+"-"+"#{params[:d1]}"+"-"+"#{params[:y1]}")
-  date2 =Date.parse("#{params[:m2]}"+"-"+"#{params[:d2]}"+"-"+"#{params[:y2]}")
-  time1 =Tiime.parse("#{params[:timestart1]}"+":"+"#{params[:timestart2]}"+"#{params[:timestart2]}")
-  time2 =Tiime.parse("#{params[:timeend1]}"+":"+"#{params[:timeend2]}"+"#{params[:timeend2]}")
+ if params[:timestart1]=="am"
+   extra1 = 0
+   else
+   extra1 = 12
+ end
+  if params[:timestart2]=="am"
+   extra2 = 0
+   else
+   extra2 = 12
+ end
+ extra1.to_i
+ extra2.to_i
+  hour1 = params[:timestart1].to_i+extra1-4
+  hour2 = params[:timeend1].to_i+extra2-4
+  
+  if params[:timestart2] == ""
+	 minute1 = 00
+	end
+  if params[:timeend2] == ""
+  minute2 = 00
+  end
+  date1 =Time.parse("#{params[:d1]}"+"-"+"#{params[:m1]}"+"-"+"#{params[:y1]}")
+  date2 =Time.parse("#{params[:d2]}"+"-"+"#{params[:m2]}"+"-"+"#{params[:y2]}")
+  time1 =Time.parse("#{hour1}"+":"+"#{minute1}"+"#{params[:timestart3]}")
+  time2 =Time.parse("#{hour2}"+":"+"#{minute2}"+"#{params[:timeend3]}")
+ 
   @userrr = Reservation.new(:reserved_user_id =>current_user.id, :name=>params[:name][:size10],
 	:location=>params[:locations], :start_date=>date1, :end_date=>date2, 
 	:start_time=>time1, :end_time=>time2, :desc=>params[:descr][:size20])
