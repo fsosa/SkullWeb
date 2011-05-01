fa1f5ed8b94fd7f5e518a7a230e0566f9d0c6cb4
Michael Patterson <mrpatte@mit.edu>
2011-05-01 13:12:49 -0400

    time works

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -26,10 +26,10 @@ def room_list
 end
 
 def create
-  date1 =[params[:m1],params[:d1],params[:y1]]
-  date2 =[params[:m2],params[:d2],params[:y2]]
-  time1 =[params[:timestart1],params[:timestart2],params[:timestart2]]
-  time2 =[params[:timeend1],params[:timeend2],params[:timeend2]]
+  date1 =Date.parse("#{params[:m1]}"+"-"+"#{params[:d1]}"+"-"+"#{params[:y1]}")
+  date2 =Date.parse("#{params[:m2]}"+"-"+"#{params[:d2]}"+"-"+"#{params[:y2]}")
+  time1 =Tiime.parse("#{params[:timestart1]}"+":"+"#{params[:timestart2]}"+"#{params[:timestart2]}")
+  time2 =Tiime.parse("#{params[:timeend1]}"+":"+"#{params[:timeend2]}"+"#{params[:timeend2]}")
   @userrr = Reservation.new(:reserved_user_id =>current_user.id, :name=>params[:name][:size10],
 	:location=>params[:locations], :start_date=>date1, :end_date=>date2, 
 	:start_time=>time1, :end_time=>time2, :desc=>params[:descr][:size20])
